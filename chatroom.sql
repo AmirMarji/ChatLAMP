a2af8b54fb568e0a3d317ba0772f8cf98a2420f4
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2023-08-17 13:56:22 -0400

    database setup

---
+++ chatroom.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 17, 2023 at 07:54 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chatroom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatrooms`
+--
+
+CREATE TABLE `chatrooms` (
+  `ChatroomID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `CreatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `MessageID` int(11) NOT NULL,
+  `ChatroomID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Content` text NOT NULL,
+  `Timestamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `CreatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chatrooms`
+--
+ALTER TABLE `chatrooms`
+  ADD PRIMARY KEY (`ChatroomID`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`MessageID`),
+  ADD KEY `ChatroomID` (`ChatroomID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`ChatroomID`) REFERENCES `chatrooms` (`ChatroomID`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- db.php
+++ db.php
@@ -1,5 +1,3 @@
 <?php
-    /* TODO work on the database and conect it together.
-       fill with a bunch of dummy data and foreach display on the landing page. 
-    */
+   //TODO connect the database and fill with dummy data.
 ?>
